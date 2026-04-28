0f8da1b021abef0601445de3ab51573f9feac467
lalasa2425479 <pallilalasa.alekhya@cognizant.com>
2026-04-28 11:56:28 +0530

    swap.py

---
+++ swap.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+temp = a
+a = b
+b = temp
+print("After swapping:")
+print("a =", a)
+print("b =", b)
